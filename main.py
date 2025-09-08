ffc7a2f1c75e4c9c8da4dcc6e1d3d98758b9ec28
M.Schardt <matthew.schardt45@gmail.com>
2025-09-08 09:18:21 -0400

    added manager and buy more buttons, not functional yet

--- main.py
+++ main.py
@@ -9,25 +9,49 @@ timer = pygame.time.Clock()
 font = pygame.font.Font("freesansbold.ttf", 16)
 
 pale_olive_value = 1
-olive_green_value = 2
-pine_needle_value = 3
-forest_green_value = 4
-dark_olive_value = 5
-dark_olive_draw = False
-pale_olive_draw = False
-olive_green_draw = False
-pine_needle_draw = False
-forest_green_draw = False
-dark_olive_length = 0
-pale_olive_length = 0
-olive_green_length = 0
-pine_needle_length = 0
-forest_green_length = 0
+pale_olive_cost = 1
 pale_olive_speed = 5
+pale_olive_manager_cost = 100
+pale_olive_owned = False
+pale_olive_draw = False
+pale_olive_length = 0
+
+olive_green_value = 2
+olive_green_cost = 2
 olive_green_speed = 4
+olive_manager_cost = 200
+olive_green_owned = False
+olive_green_draw = False
+olive_green_length = 0
+
+pine_needle_value = 3
+pine_needle_cost = 3
 pine_needle_speed = 3
+pine_manager_cost = 300
+pine_needle_owned = False
+pine_needle_draw = False
+pine_needle_length = 0
+
+forest_green_value = 4
+forest_green_cost = 4
 forest_green_speed = 2
+forest_manager_cost = 400
+forest_green_owned = False
+forest_green_draw = False
+forest_green_length = 0
+
+dark_olive_value = 5
+dark_olive_cost = 5
 dark_olive_speed = 1
+dark_manager_cost = 500
+dark_olive_owned = False
+dark_olive_draw = False
+dark_olive_length = 0
+
+
+
+
+
 score = 0
 
 
@@ -66,6 +90,17 @@ def draw_task(color, y_coord, value, draw, length, speed):
     screen.blit(val_txt, (16, y_coord - 10))
     return task, draw, length
 
+def draw_button(color, x_coord, color_cost, owned, manager_cost):
+    color_button = pygame.draw.rect(screen, color, [x_coord, 350, 50, 30])
+    color_text = font.render(str(round(color_cost,2)), True, black)
+    screen.blit(color_text, (x_coord + 6, 350))
+    if not owned:
+        manager_button = pygame.draw.rect(screen, color, [x_coord, 405, 50, 30])
+        manager_text = font.render(str(round(manager_cost,2)), True, black)
+        screen.blit(manager_text, (x_coord + 6, 410))
+    return color_button, manager_button
+
+
 running = True
 while running:
     timer.tick(frame_rate)
@@ -86,12 +121,27 @@ while running:
                 dark_olive_draw = True
     screen.fill(background)
     task1, pale_olive_draw, pale_olive_length = draw_task(pale_olive, 50, pale_olive_value, pale_olive_draw, pale_olive_length, pale_olive_speed)
+    pale_olive_buy, pale_olive_manager_buy = draw_button(pale_olive, 10, pale_olive_cost, pale_olive_owned, pale_olive_manager_cost)
+    
     task2, olive_green_draw, olive_green_length = draw_task(olive_green, 110, olive_green_value, olive_green_draw, olive_green_length, olive_green_speed)
+    olive_green_buy, olive_manager_buy = draw_button(olive_green, 70, olive_green_cost, olive_green_owned, olive_manager_cost)
+
     task3, pine_needle_draw, pine_needle_length = draw_task(pine_needle, 170, pine_needle_value, pine_needle_draw, pine_needle_length, pine_needle_speed)
+    pine_needle_buy, pine_manager_buy = draw_button(pine_needle, 130, pine_needle_cost, pine_needle_owned, pine_manager_cost)
+
     task4, forest_green_draw, forest_green_length = draw_task(forest_green, 230, forest_green_value, forest_green_draw, forest_green_length, forest_green_speed)
+    forest_green_buy, forest_manager_buy = draw_button(forest_green, 190, forest_green_cost, forest_green_owned, forest_manager_cost)   
+
     task5, dark_olive_draw, dark_olive_length = draw_task(dark_olive, 290, dark_olive_value, dark_olive_draw, dark_olive_length, dark_olive_speed)
+    dark_olive_buy, dark_manager_buy = draw_button(dark_olive, 250, dark_olive_cost, dark_olive_owned, dark_manager_cost)   
+
     display_score = font.render("Cash:" + str(round(score, 2)), True, black)
     screen.blit(display_score, (200, 10))
 
+
+    buymore = font.render("Buy More", True, black)
+    screen.blit(buymore, (10, 330))
+    hiremanager = font.render("Hire Manager", True, black)
+    screen.blit(hiremanager, (10, 385))
     pygame.display.flip()
 pygame.quit
