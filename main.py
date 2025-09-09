ffbd6264c24faf119e1d79a50f54cbd72c1ef26b
M.Schardt <matthewschardt@schardtindustries.com>
2025-09-09 08:37:50 -0400

    added more tasks and got the menu dashboard set up, not functional atm

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from color_pallete import *
 from game_variables import *
 from display_variables import *
 from draw_task import draw_task
-from draw_button import draw_button
+from draw_color_button import draw_color_button
 import os
 import json
 import datetime
@@ -75,72 +75,28 @@ while running:
                 forest_green_draw = True
             if task5.collidepoint(mouse_pos):
                 dark_olive_draw = True
-            if pale_olive_manager_buy and pale_olive_manager_buy.collidepoint(mouse_pos) and score >= pale_olive_manager_cost and not pale_olive_owned:
-                score -= pale_olive_manager_cost
-                pale_olive_owned = True
-            if olive_manager_buy and olive_manager_buy.collidepoint(mouse_pos) and score >= olive_manager_cost and not olive_green_owned:
-                score -= olive_manager_cost
-                olive_green_owned = True
-            if pine_manager_buy and pine_manager_buy.collidepoint(mouse_pos) and score >= pine_manager_cost and not pine_needle_owned:
-                score -= pine_manager_cost
-                pine_needle_owned = True
-            if forest_manager_buy and forest_manager_buy.collidepoint(mouse_pos) and score >= forest_manager_cost and not forest_green_owned:
-                score -= forest_manager_cost
-                forest_green_owned = True
-            if dark_manager_buy and dark_manager_buy.collidepoint(mouse_pos) and score >= dark_manager_cost and not dark_olive_owned:
-                score -= dark_manager_cost
-                dark_olive_owned = True
-            if pale_olive_buy and pale_olive_buy.collidepoint(mouse_pos) and score >= pale_olive_cost:
-                pale_olive_value = (pale_olive_value * 1.1)
-                score -= pale_olive_cost
-                pale_olive_cost = (pale_olive_cost * 1.1)
-            if olive_green_buy and olive_green_buy.collidepoint(mouse_pos) and score >= olive_green_cost:
-                olive_green_value = (olive_green_value * 1.15)
-                score -= olive_green_cost
-                olive_green_cost = (olive_green_cost * 1.25)
-            if pine_needle_buy and pine_needle_buy.collidepoint(mouse_pos) and score >= pine_needle_cost:
-                pine_needle_value = (pine_needle_value * 1.2)
-                score -= pine_needle_cost
-                pine_needle_cost = (pine_needle_cost * 1.5)
-            if forest_green_buy and forest_green_buy.collidepoint(mouse_pos) and score >= forest_green_cost:
-                forest_green_value = (forest_green_value * 1.3)
-                score -= forest_green_cost
-                forest_green_cost = (forest_green_cost * 1.75)
-            if dark_olive_buy and dark_olive_buy.collidepoint(mouse_pos) and score >= dark_olive_cost:
-                dark_olive_value = (dark_olive_value * 1.4)
-                score -= dark_olive_cost
-                dark_olive_cost = (dark_olive_cost * 2)
+            if task6.collidepoint(mouse_pos):
+                saddle_draw = True
+            if task7.collidepoint(mouse_pos):
+                rustic_draw = True
 
-
-            
     screen.fill(background)
 
     task1, pale_olive_draw, pale_olive_length, score = draw_task(pale_olive, 50, pale_olive_value, pale_olive_draw, pale_olive_length, pale_olive_speed, score)
-    pale_olive_buy, pale_olive_manager_buy = draw_button(pale_olive, 10, pale_olive_cost, pale_olive_owned, pale_olive_manager_cost)
-    
-    task2, olive_green_draw, olive_green_length, score = draw_task(olive_green, 110, olive_green_value, olive_green_draw, olive_green_length, olive_green_speed, score)
-    olive_green_buy, olive_manager_buy = draw_button(olive_green, 70, olive_green_cost, olive_green_owned, olive_manager_cost)
-
-    task3, pine_needle_draw, pine_needle_length, score = draw_task(pine_needle, 170, pine_needle_value, pine_needle_draw, pine_needle_length, pine_needle_speed, score)
-    pine_needle_buy, pine_manager_buy = draw_button(pine_needle, 130, pine_needle_cost, pine_needle_owned, pine_manager_cost)
-
-    task4, forest_green_draw, forest_green_length, score = draw_task(forest_green, 230, forest_green_value, forest_green_draw, forest_green_length, forest_green_speed, score)
-    forest_green_buy, forest_manager_buy = draw_button(forest_green, 190, forest_green_cost, forest_green_owned, forest_manager_cost)   
-
-    task5, dark_olive_draw, dark_olive_length, score = draw_task(dark_olive, 290, dark_olive_value, dark_olive_draw, dark_olive_length, dark_olive_speed, score)
-    dark_olive_buy, dark_manager_buy = draw_button(dark_olive, 250, dark_olive_cost, dark_olive_owned, dark_manager_cost)   
-
-    task6 = draw_to_button(mountain_gray, 5, 90, 410, 30, "Upgrades")
-    task7 = draw_to_button(mountain_gray, 105, 90, 410, 30, "Managers")
-    task8 = draw_to_button(mountain_gray, 205, 90, 410, 30, "Tasks")
+    task2, olive_green_draw, olive_green_length, score = draw_task(olive_green, 95, olive_green_value, olive_green_draw, olive_green_length, olive_green_speed, score)
+    task3, pine_needle_draw, pine_needle_length, score = draw_task(pine_needle, 140, pine_needle_value, pine_needle_draw, pine_needle_length, pine_needle_speed, score)
+    task4, forest_green_draw, forest_green_length, score = draw_task(forest_green, 185, forest_green_value, forest_green_draw, forest_green_length, forest_green_speed, score)
+    task5, dark_olive_draw, dark_olive_length, score = draw_task(dark_olive, 230, dark_olive_value, dark_olive_draw, dark_olive_length, dark_olive_speed, score)
+    task6, saddle_draw, saddle_length, score = draw_task(saddle_brown, 275, saddle_value, saddle_draw, saddle_length, saddle_speed, score)
+    task7, rustic_draw, rustic_length, score = draw_task(rustic_brown, 320, rustic_value, rustic_draw, rustic_length, rustic_speed, score)
+    task8 = draw_to_button(mountain_gray, 5, 90, 410, 30, "Upgrades")
+    task9 = draw_to_button(mountain_gray, 105, 90, 410, 30, "Managers")
+    task10 = draw_to_button(mountain_gray, 205, 90, 410, 30, "Tasks")
+    task11 = draw_to_button(mountain_gray, 10, 135, 370, 30, "Trophies")
+    task12 = draw_to_button(mountain_gray, 155, 135, 370, 30, "Store")
     display_score = font.render("Cash: $" + str(round(score, 2)), True, black)
     screen.blit(display_score, (10, 10))
 
-
-    buymore = font.render("Buy More", True, black)
-    screen.blit(buymore, (10, 330))
-    hiremanager = font.render("Hire Manager", True, black)
-    screen.blit(hiremanager, (10, 385))
     pygame.display.flip()
 
     save_data = {
